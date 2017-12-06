762a8e8443328f16517dd4739f941ea7494df63e
Jean-Philippe Roy <jp.roy.84@gmail.com>
2017-12-06 13:06:16 +0100

    quick css

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,7 +6,6 @@ const App = () => {
   return (
     <div className="app">
       <div className="logo">
-        <img src="https://images.vexels.com/media/users/3/129998/isolated/preview/a0f7721448e400ab5e2ec3bcd3b60d72-customer-agent-headshot-icon-by-vexels.png" alt=""/>
       </div>
       <Channels />
       <div className="channel"></div>
